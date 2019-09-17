8ee2569cb59ad9d595253724100a94f9a44547d2
Paloma Pineda <Paloma.Pineda@improving.com>
2019-09-17 16:09:13 -0500

    loop with exit criteria

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -4,7 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while(loop) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            input = input.trim();
+            if (input.equalsIgnoreCase("look")) {
+                System.out.println("You look around.");
+            } else if (input.equalsIgnoreCase("inventory")) {
+                System.out.println("You are carrying nothing.");
+                } else if (input.equalsIgnoreCase("dance")) {
+                System.out.println("You dance around.");
+            } else if (input.equalsIgnoreCase("jump")) {
+                System.out.println("You jump in the air.");
+            } else if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Goodbye.");
+                loop = false;
+            } else {
+                System.out.println("Huh? I don't understand.");
+            }
+        }
     }
 }
 
