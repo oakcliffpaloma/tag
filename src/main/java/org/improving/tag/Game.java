7bbf16c5d2ba701e7689a99997216a46a1fe3f54
Paloma Pineda <Paloma.Pineda@improving.com>
2019-09-19 10:08:53 -0500

    constructors and person

---
+++ src/main/java/org/improving/tag/Game.java
@@ -0,0 +1,59 @@
+package org.improving.tag;
+
+import java.util.Date;
+import java.util.Scanner;
+
+public class Game {
+    private Date startTime;
+    private Date endTime;
+
+    public Game(int maxInventory) {
+        System.out.println("Ran the Constructor, Maximum Inventory is " + maxInventory);
+    }
+    public Game(int maxInventory, int playerAge) {
+        this(maxInventory);
+        System.out.println("The player's age is " + playerAge);
+    }
+
+    public Date getStartTime(){
+        return startTime;
+    }
+
+    private void setStartTime(Date val) {
+        startTime = val;
+    }
+
+    public Date getEndTime() { return endTime; }
+        private void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        this.setStartTime(new Date());
+
+        boolean loop = true;
+        while(loop) {
+            System.out.print("> ");
+            String input = scanner.nextLine();
+            input = input.trim();
+            if (input.equalsIgnoreCase("look")) {
+                System.out.println("You look around.");
+            } else if (input.equalsIgnoreCase("inventory")) {
+                System.out.println("You are carrying nothing.");
+            } else if (input.equalsIgnoreCase("dance")) {
+                System.out.println("You dance around.");
+            } else if (input.equalsIgnoreCase("jump")) {
+                System.out.println("You jump in the air.");
+            } else if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Goodbye.");
+                loop = false;
+            } else {
+                System.out.println("Huh? I don't understand.");
+            }
+        }
+        this.setEndTime(new Date());
+    }
+
+}
--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -4,27 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean loop = true;
-        while(loop) {
-            System.out.print("> ");
-            String input = scanner.nextLine();
-            input = input.trim();
-            if (input.equalsIgnoreCase("look")) {
-                System.out.println("You look around.");
-            } else if (input.equalsIgnoreCase("inventory")) {
-                System.out.println("You are carrying nothing.");
-                } else if (input.equalsIgnoreCase("dance")) {
-                System.out.println("You dance around.");
-            } else if (input.equalsIgnoreCase("jump")) {
-                System.out.println("You jump in the air.");
-            } else if (input.equalsIgnoreCase("exit")) {
-                System.out.println("Goodbye.");
-                loop = false;
-            } else {
-                System.out.println("Huh? I don't understand.");
-            }
-        }
+        System.out.println("Start of Main");
+        Game game = new Game(100, 23);
+        System.out.println("declared Game");
+        game.run();
+        System.out.println("After run()");
+
+
+        long elapsedTicks = game.getEndTime().getTime() -
+                game.getStartTime().getTime();
+        double elapsedSeconds = elapsedTicks / 1000.0;
+        System.out.println("We were running for " + elapsedSeconds + "s.");
     }
 }
+
 
