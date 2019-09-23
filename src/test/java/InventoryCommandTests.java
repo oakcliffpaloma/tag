ba4874903cf51256ccfbcb25aac03e2013d45dd2
Paloma Pineda <Paloma.Pineda@improving.com>
2019-09-22 21:34:18 -0500

    GoingRogueCreatingTest

---
+++ src/test/java/InventoryCommandTests.java
@@ -0,0 +1,72 @@
+import org.improving.tag.commands.InventoryCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Component;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class InventoryCommandTests {
+
+    InventoryCommand target;
+    TestInputOutput io;
+
+    @BeforeEach
+    public void arrange() {
+        //Arrange
+        io = new TestInputOutput();
+        target = new InventoryCommand(io);
+    }
+    @Test
+    public void execute_should_return_phrase(){
+        //Act
+        target.execute(null);
+
+        //Assert
+        assertEquals("You are carrying nothing.", io.lastText);
+    }
+
+
+    @Test
+    public void isValid_should_be_true_when_input_is_inventory() {
+        //Act
+        var result = target.isValid("inventory");
+
+        //Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValid_should_be_false_when_input_foobar() {
+        //Act
+        var result = target.isValid("foobar");
+
+        //Assert
+        assertFalse(result);
+    }
+    @Test
+    public void isValid_should_be_false_when_input_is_null() {
+        //Act
+        var result = target.isValid( null);
+
+        //Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValid_should_be_true_when_input_is_inventory_with_spaces() {
+        //Act
+        var result = target.isValid("  inventory ");
+
+        //Assert
+        assertTrue(result);
+    }
+    @Test
+    public void isValid_should_be_true_when_input_is_inventory_with_caps() {
+        //Act
+        var result = target.isValid("inVENTory");
+
+        //Assert
+        assertTrue(result);
+    }
+}
